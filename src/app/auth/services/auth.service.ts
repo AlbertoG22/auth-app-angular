825e68ca1d8f423ed67564629701a55c0371aeac
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-09-19 17:59:17 -0600

    service to verify access token pt2

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environments } from '../../../environments/environments';
 import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -50,6 +50,21 @@ export class AuthService {
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${ token }`);
 
-    return this.http.get(url, { headers });
+    return this.http.get<CheckTokenResponse>(url, { headers })
+      .pipe(
+        map( ({ token, user }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token);
+
+          return true;
+        }),
+
+        // Error
+        catchError(() => {
+          this._authStatus.set( AuthStatus.notAuthenticated );
+          return of(false)
+        })
+      );
   }
 }
